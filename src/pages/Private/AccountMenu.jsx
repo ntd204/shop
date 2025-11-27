d2cce0100392aa38f1b91ae2daa6fbe7f395418c
Nguyễn Trọng Đạo <trongdao20402@gmail.com>
2025-11-27 14:10:37 +0700

    Update code

--- src/pages/Private/AccountMenu.jsx
+++ src/pages/Private/AccountMenu.jsx
@@ -1,58 +1,58 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Tooltip from "@mui/material/Tooltip";
-import { IoMdLogOut, IoMdPersonAdd, IoMdSettings } from "react-icons/io";
-import { useDispatch, useSelector } from "react-redux";
-import { doLogout } from "../../redux/features/authSlice";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
-import { clearWishList } from "../../redux/features/wishListSlice";
-import { clearCartItems } from "../../redux/features/cartSlice";
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Tooltip from '@mui/material/Tooltip'
+import { IoMdLogOut, IoMdPersonAdd, IoMdSettings } from 'react-icons/io'
+import { useDispatch, useSelector } from 'react-redux'
+import { doLogout } from '../../redux/features/authSlice'
+import { toast } from 'react-toastify'
+// import { useNavigate } from 'react-router-dom'
+import { clearWishList } from '../../redux/features/wishListSlice'
+import { clearCartItems } from '../../redux/features/cartSlice'
 
 export default function AccountMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const userName = useSelector((state) => state.auth.userName);
-  const isLogin = useSelector((state) => state.auth.isLogin);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const userName = useSelector((state) => state.auth.userName)
+  const isLogin = useSelector((state) => state.auth.isLogin)
+  const dispatch = useDispatch()
+  // const navigate = useNavigate()
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
   const handleLogout = () => {
-    dispatch(doLogout());
-    dispatch(clearWishList());
-    dispatch(clearCartItems());
-  };
+    dispatch(doLogout())
+    dispatch(clearWishList())
+    dispatch(clearCartItems())
+  }
   React.useEffect(() => {
     if (!isLogin) {
-      toast.success("Fail login", {
+      toast.success('Fail login', {
         autoClose: 500,
-        progress: undefined,
-      });
+        progress: undefined
+      })
     }
-  }, [isLogin]);
+  }, [isLogin])
   return (
     <React.Fragment>
-      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
         <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
             sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
+            aria-controls={open ? 'account-menu' : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
+            aria-expanded={open ? 'true' : undefined}
           >
             <Avatar sx={{ width: 32, height: 32 }}>
               {userName.slice(0, 1).toUpperCase()}
@@ -70,34 +70,37 @@ export default function AccountMenu() {
           paper: {
             elevation: 0,
             sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
               mt: 1.5,
-              "& .MuiAvatar-root": {
+              '& .MuiAvatar-root': {
                 width: 32,
                 height: 32,
                 ml: -0.5,
-                mr: 1,
+                mr: 1
               },
-              "&::before": {
+              '&::before': {
                 content: '""',
-                display: "block",
-                position: "absolute",
+                display: 'block',
+                position: 'absolute',
                 top: 0,
                 right: 14,
                 width: 10,
                 height: 10,
-                bgcolor: "background.paper",
-                transform: "translateY(-50%) rotate(45deg)",
-                zIndex: 0,
-              },
-            },
-          },
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0
+              }
+            }
+          }
         }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem onClick={() => navigate("/profile")}>
+        <MenuItem
+          onClick={handleClose}
+          //  onClick={() => navigate("/profile")}
+        >
           <Avatar /> Profile
         </MenuItem>
         <MenuItem onClick={handleClose}>
@@ -124,5 +127,5 @@ export default function AccountMenu() {
         </MenuItem>
       </Menu>
     </React.Fragment>
-  );
+  )
 }
